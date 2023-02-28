a1746fdfe9fd6e4d35d97914cf91cab2a052dde9
Brian Barbar <brianbarbar2@gmail.com>
2023-02-28 08:37:14 +0100

    Refactor sounds to be more easy to extend and less error prone

--- src/SoundManager.ts
+++ src/SoundManager.ts
@@ -16,40 +16,34 @@ export class SoundManager {
     // singleton so I can always access the SoundManager from anywhere, without needing to pass a reference
     public static I: SoundManager;
     currentlyPlayingMusic: Phaser.Sound.BaseSound;
-    
-    ballHit: Phaser.Sound.BaseSound;
-    loseLife: Phaser.Sound.BaseSound;
-    damageSound: Phaser.Sound.BaseSound;
-    wolfSounds: Array<Phaser.Sound.BaseSound>;
-    boneSpear: Array<Phaser.Sound.BaseSound>;
-    wolfBite: Phaser.Sound.BaseSound;
-    boneSpearHit: Phaser.Sound.BaseSound;
+    sounds: Record<SoundClip, Array<Phaser.Sound.BaseSound>>;
     music: Array<Phaser.Sound.BaseSound>;
-    stickDrop: Phaser.Sound.BaseSound;
-    stickCollect: Phaser.Sound.BaseSound;
     
     constructor(scene: Phaser.Scene) {
         SoundManager.I = this;
-        this.ballHit = scene.sound.add('ballHit');
-        this.damageSound = scene.sound.add('damage');
-        this.loseLife = scene.sound.add('loseLife');
-        this.wolfSounds = [
-            scene.sound.add('wolf1'),
-            scene.sound.add('wolf2'),
-            scene.sound.add('wolf3'),
-            scene.sound.add('wolf4'),
-            scene.sound.add('wolf5'),
-            scene.sound.add('wolf6'),
-        ]
-        this.wolfBite = scene.sound.add('wolfBite');
-        this.boneSpear = [
-            scene.sound.add('bonespear1'),
-            scene.sound.add('bonespear2'),
-            scene.sound.add('bonespear3'),
-        ]
-        this.boneSpearHit = scene.sound.add('bonespearhit');
-        this.stickDrop = scene.sound.add('stickDrop');
-        this.stickCollect = scene.sound.add('stickCollect');
+        this.sounds = {
+            [SoundClip.ballHit]: [scene.sound.add('ballHit')],
+            [SoundClip.damage]: [scene.sound.add('damage')],
+            [SoundClip.loseLife]: [scene.sound.add('loseLife')],
+            [SoundClip.wolf]: [
+                scene.sound.add('wolf1'),
+                scene.sound.add('wolf2'),
+                scene.sound.add('wolf3'),
+                scene.sound.add('wolf4'),
+                scene.sound.add('wolf5'),
+                scene.sound.add('wolf6'),
+            ],
+            [SoundClip.wolfBite]: [scene.sound.add('wolfBite')],
+            [SoundClip.boneSpear]: [
+                scene.sound.add('bonespear1'),
+                scene.sound.add('bonespear2'),
+                scene.sound.add('bonespear3'),
+            ],
+            [SoundClip.boneSpearHit]: [scene.sound.add('bonespearhit')],
+            [SoundClip.stickDrop]: [scene.sound.add('stickDrop')],
+            [SoundClip.stickCollect]: [scene.sound.add('stickCollect')],
+        };
+
         this.music = [
             scene.sound.add('music1', {
                 loop: true,
@@ -68,35 +62,8 @@ export class SoundManager {
     }
     
     play_sound(soundClip: SoundClip) {
-        switch (soundClip) {
-            case SoundClip.ballHit:
-                this.ballHit.play();
-                break;
-            case SoundClip.damage:
-                this.damageSound.play();
-                break;
-            case SoundClip.loseLife:
-                this.loseLife.play();
-                break;
-            case SoundClip.wolf:
-                this.wolfSounds[Math.floor(Math.random() * this.wolfSounds.length)].play();
-                break;
-            case SoundClip.wolfBite:
-                this.wolfBite.play();
-                break;
-            case SoundClip.boneSpear:
-                this.boneSpear[Math.floor(Math.random() * this.boneSpear.length)].play();
-                break;
-            case SoundClip.boneSpearHit:
-                this.boneSpearHit.play();
-                break;
-            case SoundClip.stickDrop:
-                this.stickDrop.play();
-                break;
-            case SoundClip.stickCollect:
-                this.stickCollect.play();
-                break;
-        }
+        let soundArray = this.sounds[soundClip];
+        soundArray[Math.floor(Math.random() * soundArray.length)].play();
     }
     
     play_music(level: number) {
